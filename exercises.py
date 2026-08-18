84ed4eed469ef907a2cab7efe1e455d1096f7d6c
abdullah <abdullamsuleman@gmail.com>
2026-08-18 10:05:47 +0300

    homwork

--- exercises.py
+++ exercises.py
@@ -60,4 +60,19 @@ def hometown_info():
 # Call the function and print the result
 print('Exercise 4:', hometown_info())
 
+# Exercise 5: Iterating Over Dictionary Items
+#
+# Define an empty list named home_town_items.
+# Use a for loop to iterate over the key: value pairs in the home_town dictionary and append a string with the following format to home_town_items: "<key> = <value>"
+
+def list_home_town_items():
+    # your code here
+    home_town = {"city": "austin", "state": "texas", "population": 10}
+
+    home_town_items = []
+
+    return
+
+# Call the function and print the result
+print('Exercise 5:', list_home_town_items())
 
